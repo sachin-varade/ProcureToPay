a84dca0cba95c7a47bc3adf23017d949465a56db
Melwin Dmello <melwindmello@gmail.com>
2017-12-20 21:17:15 +0530

    Vendor PO - fetch details

--- Web/src/app/vendor/po-fulfilment/po-fulfilment.component.ts
+++ Web/src/app/vendor/po-fulfilment/po-fulfilment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProcurementService } from '../../services/procurement.service';
+import * as ProcurementModels from '../../models/procurement';
 
 @Component({
   selector: 'app-po-fulfilment',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoFulfilmentComponent implements OnInit {
 
-  constructor() { }
+  purchaseOrderList: Array<ProcurementModels.PurchaseOrder> = new Array<ProcurementModels.PurchaseOrder>();
+  purchaseOrder: ProcurementModels.PurchaseOrder = new ProcurementModels.PurchaseOrder();
+  
+  constructor(private procurementService: ProcurementService) { 
+    this.fetchApprovedPOs();
+  }
 
   ngOnInit() {
+  }
+
+  fetchApprovedPOs(){
+    this.procurementService.getAllPurchaseOrders('approved')
+    .then((results: any) => {
+      this.purchaseOrderList = results.purchaseOrders;
+      if(this.purchaseOrderList){
+        this.purchaseOrder = JSON.parse(JSON.stringify(this.purchaseOrderList[0]));
+        //this.purchaseOrder.deliverToPersonName
+      }
+    });
+  }
+
+  setPO(){
+    this.purchaseOrderList.forEach(element => {
+      if(element.purchaseOrderNumber === this.purchaseOrder.purchaseOrderNumber){
+        this.purchaseOrder = JSON.parse(JSON.stringify(element));
+      }
+    });
   }
 
 }
