ec2347f12133f49ac5d5112daf5b114ef19ec72e
Nirmal Patel <nirmal.06it37@gmail.com>
2017-12-27 12:24:11 +0000

    vendor invoice model

--- Web/src/app/models/vendor.ts
+++ Web/src/app/models/vendor.ts
@@ -30,3 +30,27 @@ export class  VendorMaterial {
 	NetAmount: Number
 	DispatchedQuantity: Number;
 }
+
+export class  VendorInvoice {
+	invoiceNumber: string;
+	invoiceDate: Date;
+	goodsIssueNumber: string;
+	goodsIssueDate: Date;
+	salesOrderNumber: string;
+	purchaseOrderRefNumber: string;
+	purchaseOrderRefDate: Date;
+	supplierCode: string;
+	purchaserCompany: string;
+	purchaserCompanyDept: string;
+	purchaserContactPersonName: string;
+	purchaserContactPersonAddress: string;
+	purchaserContactPersonPhone: string;
+	purchaserContactPersonEmail: string;
+	deliverToPersonName: string;
+	deliveryAddress: string;
+	invoicePartyId: string;
+	invoiceAddress: string;
+	grossAmount: number;
+	vatNumber: string;
+	materialList: Array<VendorMaterial> = new Array<VendorMaterial>();
+}
